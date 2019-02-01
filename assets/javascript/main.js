db8d317d4ff461bcffe2935a6fd7d50dc5d39bbd
Jameson Aardalen <jamesonaardalen@gmail.com>
2019-02-01 02:47:52 -0800

    second commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,32 @@
-apiKey = "AIzaSyBbymCZs4uNMXULkJ7JbUv1Ucp4_WeyuuY";
+$(document).ready(function() {
+
+    $(".flower-section").hide();
+    $(".vapes-section").hide();
+    $(".coming-soon").hide();
+
+    $("#flower-icon").click(function() {
+        $(".vapes-section").hide();
+        $(".coming-soon").hide();
+        $(".flower-section").toggle();
+    });
+
+    $("#vapes-icon").click(function() {
+        $(".flower-section").hide();
+        $(".coming-soon").hide();
+        $(".vapes-section").toggle();
+    });
+
+    $("#concentrates-icon").click(function() {
+        $(".vapes-section").hide();
+        $(".flower-section").hide();
+        $(".coming-soon").toggle();
+    });
+
+    $("#edibles-icon").click(function() {
+        $(".vapes-section").hide();
+        $(".flower-section").hide();
+        $(".coming-soon").toggle();
+    });
+    
+
+});
