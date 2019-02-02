b04ae16f355555efdd10cad9b3d181b450ccfe20
Jameson Aardalen <jamesonaardalen@gmail.com>
2019-02-01 19:32:40 -0800

    another one

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,24 +5,35 @@ $(document).ready(function() {
     $(".coming-soon").hide();
 
     $("#flower-icon").click(function() {
+        $(".tinctures").hide();
         $(".vapes-section").hide();
         $(".coming-soon").hide();
         $(".flower-section").toggle();
     });
 
     $("#vapes-icon").click(function() {
+        $(".tinctures").hide();
         $(".flower-section").hide();
         $(".coming-soon").hide();
         $(".vapes-section").toggle();
     });
 
     $("#concentrates-icon").click(function() {
+        $(".tinctures").hide();
         $(".vapes-section").hide();
         $(".flower-section").hide();
         $(".coming-soon").toggle();
     });
 
     $("#edibles-icon").click(function() {
+        $(".tinctures").hide();
+        $(".vapes-section").hide();
+        $(".flower-section").hide();
+        $(".coming-soon").toggle();
+    });
+
+    $("#tinctures-icon").click(function() {
+        $(".edibles-section").hide();
         $(".vapes-section").hide();
         $(".flower-section").hide();
         $(".coming-soon").toggle();
